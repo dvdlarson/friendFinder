26ecdffd927c01f2059fd3c4ebac580f218eb2b8
unknown <webd3vdave@gmail.com>
2018-04-29 22:47:10 -0700

    progress. survey page not working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,47 @@
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
 
-var friendsArray = [
-    
+  module.exports = [
+
+    {"name":"Bob Dobbs",
+    "photoURL":"https://upload.wikimedia.org/wikipedia/en/a/a3/Bobdobbs.png",
+    "scores":[5,5,5,5,5,5,5,5,5,5]},
+
+    {"name":"Gordon Ramsay",
+    "photoURL":"https://cdn.vox-cdn.com/thumbor/T8d_mFfsglljnRVA66XAtYgMdHk=/0x8:468x320/920x613/filters:focal(0x8:468x320)/cdn.vox-cdn.com/uploads/chorus_image/image/37112038/ramsay1.0.0.jpg",
+    "scores":[4,3,5,3,1,2,3,4,5,4]},
+
+    {"name":"Neil Tyson",
+    "photoURL":"http://4.bp.blogspot.com/-lBcfr_hzTnY/Uygpp1qV5cI/AAAAAAAADtg/IlA68JUBhsc/s1600/neil-degrasse-tyson-tells-us-why-star-trek-is-so-much-better-than-star-wars.jpg",
+    "scores":[2,3,4,5,1,2,2,2,3,3]},
+
+    {"name":"Judith Reed",
+    "photoURL":"https://mrlongdrag.files.wordpress.com/2017/03/all-these-flavors.png",
+    "scores":[4,4,3,3,2,4,5,4,2,2]},
+
+    {"name":"Henrietta Carroll",
+    "photoURL":"http://ofad.org/files/imagecache/daily_picture/daily-photo/ofad-photo-of-8845.jpg",
+    "scores":[3,4,5,3,3,2,2,2,1,3]},
+
+    {"name":"Jimmy Crosier",
+    "photoURL":"https://vignette.wikia.nocookie.net/althistory/images/7/74/Recent-portraits-random-people-in-random-places_11.jpg/revision/latest?cb=20141209232111",
+    "scores":[4,3,2,2,4,5,4,4,5,4]},
+
+    {"name":"Mary Buckner",
+    "photoURL":"http://www.bajiroo.com/wp-content/uploads/2013/05/funny_fun_humor_people_pics_images_photos_pictures_43.jpg",
+    "scores":[3,3,3,4,5,2,2,2,1,2]},
+
+    {"name":"Paul Carson",
+    "photoURL":"http://www.bajiroo.com/wp-content/uploads/2013/05/funny_fun_humor_people_pics_images_photos_pictures_2.jpg",
+    "scores":[5,4,3,2,4,5,5,4,3,2]},
+
+    {"name":"Michael Boren",
+    "photoURL":"http://ofad.org/files/imagecache/daily_picture/daily-photo/ofad-photo-of-3287_0.jpg",
+    "scores":[4,3,2,5,4,4,1,1,1,2]},
+
+    {"name":"Sherry Anderson",
+    "photoURL":"http://ofad.org/files/daily-photo/ofad-photo-of-1280.jpg",
+    "scores":[5,5,5,5,4,4,4,4,3,3]}
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = tableArray;
+
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,10 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData = require("../data/friends");
-var waitListData = require("../data/waitinglistData");
+var friendData = require("../data/friends.js");
+var path = require("path");
 
+var answerDifference=0;
 
 // ===============================================================================
 // ROUTING
@@ -14,45 +15,67 @@ var waitListData = require("../data/waitinglistData");
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+    return res.json(friendData);
   });
 
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
+  // handles when a user submits the form and submits survey data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+ 
     
     // req.body is available since we're using the body-parser middleware
    
-    //addUser(req.body) ==> send the new record to the database
-    //getUsers() ==> get current array of users from DB
-      friendData.push(req.body);
-      res.json(true);
 
-    
+	app.post('/api/friends', function(req, res){
+
+		var matchPerson = {
+			name: "",
+			image: "",
+			matchDifference: 1000
+		};
+		var userData 	= req.body;
+		var userName 	= userData.name;
+		var userImage 	= userData.image;
+		var uerScores 	= userData.scores;
+
+		var totalDifference = 0;
+
+		//loop through the friends data array of objects to get each friends scores
+		for(var i = 0; i < [friends].length-1; i++){
+      console.log(friends[i].name);
+			totalDifference = 0;
+
+			//loop through that friends score and the users score and calculate the 
+			// absolute difference between the two and push that to the total difference variable set above
+			for(var j = 0; j < 10; j++){
+				// calculate difference between the scores 
+				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
+				// If the sum of differences is less than current "best match"
+				if (totalDifference <= matchPerson.matchDifference){
+
+					// Reset the bestMatch to be the current friend. 
+					matchPerson.name = friends[i].name;
+					matchPerson.photo = friends[i].photoURL;
+					matchPerson.matchDifference = totalDifference;
+				}
+			}
+		}
+//addUser(req.body) ==> send the new record to the database
+		friends.push(userData);
+ 
+		res.json(matchPerson);
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
 };
